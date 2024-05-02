ff39e8d38ae6fea9b6df7fdddaa0e2aa8bc97559
gpwclark <gpwclark@gmail.com>
2024-05-02 17:41:26 +0000

    store

--- apply-patch.sh
+++ apply-patch.sh
@@ -3,4 +3,5 @@
 git apply st-fix-keyboard-input-20180605-dc3b5ba.diff
 git apply st-workingdir-20200317-51e19ea.diff
 git apply xrandrfontsize-0.8.4-20211224-2f6e597.diff
+git apply st-cyberpunk-neon-20220703-baa9357.diff
 
